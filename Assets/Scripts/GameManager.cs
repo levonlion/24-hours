dcba67be37d0b3d799951eabff9da6a9bd4875e2
Levon Lion <levonlionk@gmail.com>
2023-05-22 00:42:22 +0400

    Minor changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,9 @@
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using Random = UnityEngine.Random;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;  
 
 public class GameManager : MonoBehaviour
 {
@@ -15,7 +18,12 @@ public class GameManager : MonoBehaviour
     [SerializeField] private StatController _karmaController;
     [SerializeField] private StatController _healthController;
     [SerializeField] private StatController _timeController;
-    
+    [SerializeField] private GameObject _startMenuPanel;
+    [SerializeField] private Image _dimming;
+
+    [SerializeField] private GameObject _endscreenPanel;
+    [SerializeField] private TMP_Text _endScreenText;
+
     private NodeController _currentNodeController;
     private List<string> _conditions = new List<string>();
 
@@ -23,9 +31,80 @@ public class GameManager : MonoBehaviour
     {
         Instance = this;
         UpdateCurrentNode(_startingNode);
-        _karmaController.Initialize(_startingStats.Karma, _maxStats.Karma, "Karma");
-        _healthController.Initialize(_startingStats.Health, _maxStats.Health, "Health");
-        _timeController.Initialize(_startingStats.Time, _maxStats.Time, "Time");
+        _karmaController.Initialize(_startingStats.Karma, _maxStats.Karma, "Karma", false);
+        _healthController.Initialize(_startingStats.Health, _maxStats.Health, "Health", false);
+        _timeController.Initialize(_startingStats.Time, _maxStats.Time, "Time", true);
+        _karmaController.OnValueChanged += () => _endScreenText.text = _karmaController.Value >= 50 ? "Вы попали в рай" : "Вы попали в ад";
+    }   
+
+    public void StartGame()
+    {
+        _dimming.gameObject.SetActive(true);
+        StartCoroutine(Dim());
+
+        IEnumerator Dim()
+        {
+            
+            float t = 0;
+
+            do
+            {
+                var col = Color.black;
+                col.a = t;
+                _dimming.color = col;
+                t += Time.deltaTime;
+                yield return null;
+            } while (t < 1);
+
+            _startMenuPanel.SetActive(false);
+            yield return new WaitForSeconds(1);
+            
+            do
+            {
+                var col = Color.black;
+                col.a = t;
+                _dimming.color = col;
+                t -= Time.deltaTime;
+                yield return null;
+            } while (t > 0);
+            
+            _dimming.gameObject.SetActive(false);
+        }
+    }
+
+    public void Lose()
+    {
+        StartCoroutine(Dim());
+
+        IEnumerator Dim()
+        {
+            _dimming.gameObject.SetActive(true);
+            float t = 0;
+
+            do
+            {
+                var col = Color.black;
+                col.a = t;
+                _dimming.color = col;
+                t += Time.deltaTime;
+                yield return null;
+            } while (t < 1);
+
+            _endscreenPanel.SetActive(true);
+            
+            yield return new WaitForSeconds(1);
+            
+            do
+            {
+                var col = Color.black;
+                col.a = t;
+                _dimming.color = col;
+                t -= Time.deltaTime;
+                yield return null;
+            } while (t > 0);
+            
+            _dimming.gameObject.SetActive(false);
+        }
     }
 
     public void SwipeLeft()
--- Assets/Scripts/StatController.cs
+++ Assets/Scripts/StatController.cs
@@ -1,27 +1,54 @@
+using System;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class StatController : MonoBehaviour
 {
+    public event Action OnValueChanged;
     public int Value { get; private set; }
     public int MaxValue { get; private set; }
-
+    
+    private bool _isTime;
+    
     [SerializeField] private Slider _slider;
     [SerializeField] private TMP_Text _text;
+    [SerializeField] private TMP_Text _endscreenText;
 
-    public void Initialize(int startValue, int maxValue, string name)
+    public void Initialize(int startValue, int maxValue, string name, bool isTime)
     {
         Value = startValue;
         MaxValue = maxValue;
         _text.text = name;
-        
+        _isTime = isTime;
+
         _slider.value = (float)Value / MaxValue;
+        
+        if(isTime)
+            UpdateTime();
     }
     
     public void Add(int value)
     {
         Value += value;
+        Value = Mathf.Min(Value, MaxValue);
         _slider.value = (float)Value / MaxValue;
+        
+        if (_isTime)
+            UpdateTime();
+        else
+            _endscreenText.text = (Mathf.Max(Value, 0)).ToString();
+
+        OnValueChanged?.Invoke();
+
+        if (Value <= 0)
+            GameManager.Instance.Lose();
+    }
+    
+    private void UpdateTime()
+    {
+        var span = TimeSpan.FromMinutes(Value).ToString(@"hh\:mm");;
+        _text.text = (Value < 0 ? "-" : " ") + span;
     }
 }
